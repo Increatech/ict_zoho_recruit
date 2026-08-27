9c17378f5c80c38456d79e97f43cc178abc67784
sivakumard <sivakumar.d@increatech.com>
2026-08-27 10:09:03 +0530

    chore : code update

--- ict_zoho_recruit/api/ZohoRecruit.py
+++ ict_zoho_recruit/api/ZohoRecruit.py
@@ -113,13 +113,12 @@ def sync_zoho_recruit(document_ids, operation="create"):
                 response = service._create_Job_Openings(
                     request_payload=payload
                 )
-                service._upload_attachment(job_post)
+                
             else:
                 response = service._update_Job_Openings(
                     request_payload=payload,
                     id=zoho_id,
                 )
-                service._upload_attachment(job_post)
 
             data = response.get("data") or []
             result = data[0] if data else {}
@@ -152,7 +151,9 @@ def sync_zoho_recruit(document_ids, operation="create"):
                     },
                     update_modified=True,
                 )
-
+                
+            service._upload_attachment(job_post)
+            
             results.append({
                 "document_id": doc_id,
                 "status": "success",
